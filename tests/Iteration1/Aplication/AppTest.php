8f445408e7d8e84804537f052f259be639c4c5ae
julietaCuadrado <julieta.martinez.cuadrado@gmail.com>
2018-05-16 08:59:37 +0200

    test skeleton

---
+++ tests/Iteration1/Aplication/AppTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Php version 7.1.17
+ * FactoryKat(j)a by julietaCuadrado
+ */
+
+use PHPUnit\Framework\TestCase;
+use FactoryKatja\Application\App;
+
+/**
+ * Class TestWeapon
+ */
+class AppTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function shouldRun()
+    {
+        $this->assertEquals(true, true);
+    }
+}
